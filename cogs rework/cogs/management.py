4d19e268996af1be4acb08be5373a1b5191e682e
lubnc4261 <70036080+lubnc4261@users.noreply.github.com>
2021-06-06 12:29:19 +0200

    fixed wrong word in help box

--- cogs rework/cogs/management.py
+++ cogs rework/cogs/management.py
@@ -30,7 +30,7 @@ class managementCog(commands.Cog):
         embed.add_field(name=":mechanic: `name <name>`", value="Changes the Server name", inline=True)
         embed.add_field(name=":technologist: `poll <text-for-poll> <vote1> <vote2>`", value="Make a poll", inline=True)
         embed.add_field(name=":paperclip: `log <limit>`", value="Saves the chat and send the File to chat", inline=True)
-        embed.add_field(name=":man_mage: `delcat <channel>`", value="Delete a category", inline=True)
+        embed.add_field(name=":man_mage: `delcat <category>`", value="Delete a category", inline=True)
         embed.set_footer(text="Help Box for the House Keeper Bot")
 
         await ctx.send(embed = embed)
