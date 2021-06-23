38799aa6a846c37efa86040213a34ee11f247050
lubnc4261 <70036080+lubnc4261@users.noreply.github.com>
2021-06-23 21:23:37 +0200

    added more events
    raw copy pasted the whole file

--- cogs rework/cogs/event.py
+++ cogs rework/cogs/event.py
@@ -1,9 +1,14 @@
 import discord
+from discord import Member
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions, BadArgument, MissingRequiredArgument, CommandInvokeError, has_permissions
 import json
 import re
+import os
+from datetime import datetime
 from time import gmtime, strftime
+
+
 
 def get_prefix(bot, message):
     if not message.guild:
@@ -22,6 +27,7 @@ client = commands.Bot(command_prefix=get_prefix)
 
 
 
+
 class eventCog(commands.Cog):
 	def __init__(self, bot):
 		self.client = bot
@@ -35,7 +41,7 @@ class eventCog(commands.Cog):
 		if urls and str(message.channel.id) in open ("idstuff/links/deny.py").read():
 			await message.delete()
 			await message.channel.send("Link detected and deleted")
-			
+
 	@commands.Cog.listener()
 	async def on_message(self, message):
 		attach = message.attachments
@@ -64,6 +70,7 @@ class eventCog(commands.Cog):
 
 				else:
 					pass
+
 			
 
 
@@ -77,6 +84,7 @@ class eventCog(commands.Cog):
 			embed = discord.Embed(
 				title=f"Message got deleted",
 				color = discord.Color.purple(),
+				timestamp = datetime.utcnow()
 				
 			)
 
@@ -90,25 +98,206 @@ class eventCog(commands.Cog):
 
 			await channel.send(embed=embed)
 			await client.process_commands(message)
+
+
 		
-		except Exception as e:
-			print(str(e))
+		except AttributeError:     # makes us ignore an exception that occures when the channel doesn't get found
+			pass
+
+		except discord.Forbidden:  # stop the console from spams because if the bot get's removed from a guild it could still process sth. that would raise this exception
+			pass
+
 
 
 	@commands.Cog.listener()
 	async def on_message_edit(self, before, after):
 		pass
 
+	@commands.Cog.listener()
+	async def on_guild_channel_create(self, channel):
+		try:
+
+			chann = discord.utils.get(channel.guild.channels, name="hk-logging")
+			time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+			embed = discord.Embed(
+				title="Channel got created",
+				color = discord.Color.green(),
+				timestamp = datetime.utcnow()
+			)
+
+			embed.add_field(name="Channel:", value=f"**{channel}**")
 
 
+			await chann.send(embed=embed)
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+
+	@commands.Cog.listener()
+	async def on_guild_channel_delete(self, channel):
+		try:
+
+			chann = discord.utils.get(channel.guild.channels, name="hk-logging")
+			time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+			embed = discord.Embed(
+				title="Channel got deleted",
+				color = discord.Color.red(),
+				timestamp = datetime.utcnow()
+			)
+
+			embed.add_field(name="Channel:", value=f"**{channel}**")
+	
+
+			await chann.send(embed=embed)
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+	@commands.Cog.listener()
+	async def on_member_update(self, before: Member, after: Member):
+		try:
+
+			chann = discord.utils.get(before.guild.channels, name="hk-logging")
+			if before.display_name != after.display_name:
+				embed = discord.Embed(
+				title="Nickname update occured",
+				color = discord.Color.purple(),
+				timestamp = datetime.utcnow()
+				)
+
+				embed.add_field(name="before", value=f"{before.display_name}")
+				embed.add_field(name="after", value=f"{before.display_name}")
+
+				await chann.send(embed=embed)
+
+			elif before.roles != after.roles:
+				embed = discord.Embed(
+					title="Role update",
+					color = discord.Color.red(),
+					timestamp = datetime.utcnow()
+				)
+
+				fields = [("before", ", ".join([r.mention for r in before.roles]), False),
+						("after", ", ".join([r.mention for r in after.roles]), False)]
+
+				for name, value, inline in fields:
+					embed.add_field(name=name, value=value, inline=inline)
+
+				await chann.send(embed=embed)
+
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+
+
+	@commands.Cog.listener()
+	async def on_guild_role_create(self, role):
+		try:
+
+			chann = discord.utils.get(role.guild.channels, name="hk-logging")
+			embed = discord.Embed(
+			title="Role got created",
+			color = discord.Color.green(),
+			timestamp = datetime.utcnow()
+			)
+
+			embed.add_field(name="Role", value=f"{role}")
+		
+			await chann.send(embed=embed)
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+	@commands.Cog.listener()
+	async def on_guild_role_delete(self, role):
+		try:
+
+			chann = discord.utils.get(role.guild.channels, name="hk-logging")
+			embed = discord.Embed(
+			title="Role got deleted",
+			color = discord.Color.red(),
+			timestamp = datetime.utcnow()
+			)
+
+			embed.add_field(name="Role", value=f"{role}")
+
+			await chann.send(embed=embed)
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+
+	@commands.Cog.listener()
+	async def on_member_ban(self, guild, user):
+		try:
+
+			chann = discord.utils.get(guild.channels, name="hk-logging")
+
+			embed = discord.Embed(
+			title="User ban",
+			color = discord.Color.red(),
+			timestamp = datetime.utcnow()
+			)
+
+			embed.add_field(name="\u200b", value=f"{user.mention} got banned") #\u200b makes empty field
+
+			await chann.send(embed=embed)
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+	@commands.Cog.listener()
+	async def on_member_unban(self, guild, user):
+		try:
+
+			chann = discord.utils.get(guild.channels, name="hk-logging")
+
+			embed = discord.Embed(
+			title="User unban",
+			color = discord.Color.green(),
+			timestamp = datetime.utcnow()
+			)
+
+			embed.add_field(name="\u200b", value=f"{user.mention} got unbanned")
+
+			await chann.send(embed=embed)
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
 
 
 	@commands.Cog.listener()
 	async def on_guild_join(self, guild):
 
 		if guild.system_channel:
-			await guild.system_channel.send(f"Thanks for inviting me to {guild.name} ! \nYou can see all my commands by typing §§help\nYou can change the prefix any time by using ``<prefix>prefix<new_prefix>`` \nTo make sure every command will work you can additionally give me Admin permissions\nI need to have all Permissions from the 0Auth Scope to work properly")
-		
+			message = f"Thanks for adding me to {guild.name}\n``-``Use ``<help`` to view all my commands\n``-``You can change my prefix from ``<`` by using ``<prefix <new prefix>``\n``-``If you need more help, you can visit my support server at\nhttps://www.discord.gg/gJaZbKCmE7\n▬▬▬▬▬▬▬▬▬▬▬▬▬\nI support moderation commands as well as other commands. To provide that all my commands work properly, you can additionally give me Administrator permissions\nI also have a logging function so if you run ``<setuplog`` i will log events in it\nCurrent logging cases are:\n**-** deleteed messages\n**-** channel creation\n**-** channel delete\n**-** nickname update\n**-** role update\n**-** role creation\n**-** role delete\n**-** member ban\n**-** member unban\n**-** member join\n**-** member remove."
+			await guild.system_channel.send(message)
+
 		else:
 			pass
 
@@ -116,11 +305,24 @@ class eventCog(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_guild_remove(self, guild):
-		print("removed from {guild}")
+		print(f"Removed from {guild}")
+
+
 
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
-		pass
+		try:
+
+			channel = discord.utils.get(member.guild.channels, name="hk-logging")
+			await channel.send(f"{member.mention} has joined")
+
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+
 
 	@commands.Cog.listener()
 	async def on_member_remove(self, member):
@@ -129,8 +331,34 @@ class eventCog(commands.Cog):
 			channel = discord.utils.get(member.guild.channels, name="hk-logging")
 			await channel.send(f"{member.mention} has left")
 
-		except Exception as a:
-			print(a)
+		except AttributeError:
+			pass
+
+		except discord.Forbidden:
+			pass
+
+
+
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx, error):
+		if isinstance(error, commands.CommandNotFound):
+			pass
+
+		elif isinstance(error, commands.DisabledCommand):
+			await ctx.send("This command is disabled")
+
+		elif isinstance(error, commands.NoPrivateMessage):
+			await ctx.send("This cannot be used in DM's")
+
+		elif isinstance(error, commands.MissingRequiredArgument):
+			await ctx.send("Missing Argument")
+
+		elif isinstance(error, commands.MissingAnyRole):
+			await ctx.send("Missing any Role")
+
+		else:
+			pass
+
 
 
 
