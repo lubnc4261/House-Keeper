89ea9ebb822f1cfd878e6c49e3fe9414d87bdc45
lubnc4261 <70036080+lubnc4261@users.noreply.github.com>
2024-08-07 01:04:00 +0200

    Update cmd.py

--- cogs/cmd.py
+++ cogs/cmd.py
@@ -18,6 +18,7 @@ class helpCog(commands.Cog):
             embed.add_field(name=":paperclip: **utilities**\n``utilities``", value="Commands for usefull utilities", inline=True)
             embed.add_field(name=":smile: **fun**\n``fun``", value="Bot commands for funny stuff", inline=True)
             embed.add_field(name=":joystick: **games**\n``game``", value="Play games with the bot", inline=True)
+            embed.add_field(name=":magic_wand: **slash**\n``slash``", value="Lists all slash interactions", inline=True)
             embed.add_field(name=":desktop: **bot**\n``bot``", value="Commands for the Bot itself", inline=True)
             embed.set_footer(text="Help Box for the House Keeper Bot")
 
